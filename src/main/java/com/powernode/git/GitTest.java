f753a568efc2e52d54a3e4c4f610da1a2510e9a1
qianbinglin <qianbinglin@example.com>
2025-06-10 10:38:52 +0800

    hot-fix  commit1

--- src/main/java/com/powernode/git/GitTest.java
+++ src/main/java/com/powernode/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
 		System.out.println("hello world!!");
 		System.out.println("hello world!!2");
 		System.out.println("hello world!!3");
+		System.out.println("hello world!!hot-fix 1");
 	}
 
 }
