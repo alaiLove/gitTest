f774dddc4a78508a0da40990cc4d4a61093260b1
qianbinglin <qianbinglin@example.com>
2025-06-10 10:02:32 +0800

    first commit

---
+++ src/main/java/com/powernode/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.powernode.git;
+
+/**
+ * @projectName:
+ * @author:qianbinglin
+ * @date:2025/6/10 9:54
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("hello world!!");
+	}
+
+}
