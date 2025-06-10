cfb5d6358d9ec7f7b3adaf455fd7468abbaf6a98
alaiLove <160747136+alaiLove@users.noreply.github.com>
2025-06-10 12:24:15 +0800

    pull commit

--- src/main/java/com/powernode/git/GitTest.java
+++ src/main/java/com/powernode/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
 		System.out.println("hello world!!hot-fix 1");
 		System.out.println("hello world!!master1");
 		System.out.println("hello world!!hot-fix 2");
+		System.out.println("hello world!!pull test");
 	}
 
 }
